94e0e81c055eccd5e37d04585c40995da5ae6d6b
Luaskas <ewert.lukas@outlook.de>
2024-02-29 22:02:41 +0100

    PlayerInputAction added
    First InputAction added for Playermovement.
    Player is not able yet, to actually move.

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private PlayerInputAction input = null;
+    private Vector2 moveVector = Vector2.zero;
+
+    private void Awake()
+    {
+        input = new PlayerInputAction();
+    }
+
+    private void OnEnable()
+    {
+        input.Enable();
+        input.Player.Movement.performed += OnMovementPerformed;
+        input.Player.Movement.canceled += OnMovementCancelled;
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+        input.Player.Movement.performed -= OnMovementPerformed;
+        input.Player.Movement.canceled -= OnMovementCancelled;
+    }
+
+    private void FixedUpdate()
+    {
+       
+    }
+
+    private void OnMovementPerformed(InputAction.CallbackContext value)
+    {
+        moveVector = value.ReadValue<Vector2>();
+    }
+
+    private void OnMovementCancelled(InputAction.CallbackContext value)
+    {
+        moveVector = Vector2.zero;
+    }
+}
